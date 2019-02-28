83e15039ad9fa2aa23def97b5e31791791c78f61
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-27 19:01:21 -0800

    Moved sortByProperty

--- test/sort-contacts.test.js
+++ test/sort-contacts.test.js
@@ -1,19 +1,8 @@
+import sortByProperty from '../src/sort-by-property.js';
+
 const test = QUnit.test;
 
 QUnit.module('sort contacts');
-
-function sortByProperty(contacts, sortChoices) {
-    const property = sortChoices.property;
-    return contacts.sort((a, b) => {
-        if(a[property] === b[property]) {
-            return 0;
-        }
-        if(a[property] > b[property]) {
-            return 1;
-        }
-        return -1;
-    });
-}
 
 const contacts = [
     { firstName: 'Andrew', balance: 5 },
