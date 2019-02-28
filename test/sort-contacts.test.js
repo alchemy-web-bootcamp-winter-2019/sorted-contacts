616de34fac2e572f214595bd818da0a907c8d610
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-27 18:03:55 -0800

    started sorting test

--- test/sort-contacts.test.js
+++ test/sort-contacts.test.js
@@ -1,1 +1,20 @@
 const test = QUnit.test;
+
+test('sort by first name', assert => {
+    //arrange
+    const contacts = {
+        name: 'Megan',
+        age: 1
+    },
+    {
+        name: 'Erin',
+        age: 15
+    },
+    {
+        name: 'Patrick',
+        age: 25
+    }
+    //act
+
+    //assert
+});
