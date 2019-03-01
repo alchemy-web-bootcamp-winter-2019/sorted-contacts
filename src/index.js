47655d00622f362b657d41306f184ad6e987096d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-28 17:18:13 -0800

    added loadSort function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import contacts from '../data/contacts.js';
 import loadContacts from './contacts-component.js';
+import loadSort from './sort-component.js';
 
 loadContacts(contacts);
+
+loadSort();
