fdcb55cc90ef0e202af8d81bb1f881b18f63e58e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-01 15:45:30 -0800

    passed first test now must inject template literals into function

--- test/contacts-component.test.js
+++ test/contacts-component.test.js
@@ -1,26 +1,53 @@
 const test = QUnit.test;
 
-QUnit.module('makeContactRow function');
+QUnit.module('makeContactRows function');
 
-test('make contact row template', assert => {
+function makeContactRows(){
+    const html = /*html*/ `<tr>
+    <td>5c75a7b05ef279a15486ff42</td>
+    <td>Yes</td>
+    <td>2599.99</td>
+    <td>
+        <img src="http://placehold.it/32x32">
+    </td>
+    <td>28 yo</td>
+    <td>Cantu</td>
+    <td>Sullivan</td>
+    <td>IDETICA</td>
+    <td>cantu.sullivan@idetica.com</td>
+    <td>banana</td>            
+    </tr>`;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    
+    return template.content;
+}
+
+
+test('make contact row template', function(assert) {
 //arrange
-const singleContact = /*html*/ `<tr>
-<td>5c75a7b05ef279a15486ff42</td>
-<td>Yes</td>
-<td>2599.99</td>
-<td>
-    <img src="http://placehold.it/32x32">
-</td>
-<td>28 yo</td>
-<td>Cantu</td>
-<td>Sullivan</td>
-<td>IDETICA</td>
-<td>cantu.sullivan@idetica.com</td>
-<td>banana</td>            
-</tr>`;
+    const singleContact = {
+        
+    };
 //act
+    const result = makeContactRows(singleContact);
 
+    const expected = /*html*/`<tr>
+        <td>5c75a7b05ef279a15486ff42</td>
+        <td>Yes</td>
+        <td>2599.99</td>
+        <td>
+            <img src="http://placehold.it/32x32">
+        </td>
+        <td>28 yo</td>
+        <td>Cantu</td>
+        <td>Sullivan</td>
+        <td>IDETICA</td>
+        <td>cantu.sullivan@idetica.com</td>
+        <td>banana</td>            
+        </tr>`;
 //assert
-assert.deepEqual(expected, result);
+    assert.htmlEqual(result, expected);
 
 });
