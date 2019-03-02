fa25216df32afb6375d37e52764af1ea03c5170e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-01 16:01:00 -0800

    created makeContactRows function in test suite

--- test/contacts-component.test.js
+++ test/contacts-component.test.js
@@ -2,20 +2,20 @@ const test = QUnit.test;
 
 QUnit.module('makeContactRows function');
 
-function makeContactRows(){
+function makeContactRows(singleContact){
     const html = /*html*/ `<tr>
-    <td>5c75a7b05ef279a15486ff42</td>
-    <td>Yes</td>
-    <td>2599.99</td>
+    <td>${singleContact.id}</td>
+    <td>${singleContact.isActive ? 'yes' : 'no'}</td>
+    <td>${singleContact.balance}</td>
     <td>
-        <img src="http://placehold.it/32x32">
+        <img src="${singleContact.picture}">
     </td>
-    <td>28 yo</td>
-    <td>Cantu</td>
-    <td>Sullivan</td>
-    <td>IDETICA</td>
-    <td>cantu.sullivan@idetica.com</td>
-    <td>banana</td>            
+    <td>${singleContact.age} yo</td>
+    <td>${singleContact.firstName}</td>
+    <td>${singleContact.lastName}</td>
+    <td>${singleContact.company}</td>
+    <td>${singleContact.email}</td>
+    <td>${singleContact.favoriteFruit}</td>            
     </tr>`;
 
     const template = document.createElement('template');
@@ -28,24 +28,37 @@ function makeContactRows(){
 test('make contact row template', function(assert) {
 //arrange
     const singleContact = {
-        
+        "_id": "5c75a7b05ef279a15486ff42",
+        "isActive": true,
+        "balance": 2559.99,
+        "picture": "http://placehold.it/32x32",
+        "age": 28,
+        "eyeColor": "blue",
+        "firstName": "Cantu",
+        "lastName": "Sullivan",
+        "company": "IDETICA",
+        "email": "cantu.sullivan@idetica.com",
+        "phone": "+1 (970) 454-3315",
+        "address": "590 Pershing Loop, Sabillasville, Pennsylvania, 2892",
+        "about": "Minim exercitation nisi ullamco consequat fugiat aute sit amet aute eu nisi. Nulla ad dolor ut fugiat sit sint commodo. Sint duis ullamco labore aute pariatur aute velit consequat elit proident sit aute est. In in culpa velit quis enim occaecat. Veniam esse laboris irure consequat. Laborum dolore nulla excepteur aute sit minim sit voluptate labore non esse cupidatat exercitation. Eiusmod incididunt consectetur quis proident.",
+        "favoriteFruit": "banana"
     };
 //act
     const result = makeContactRows(singleContact);
 
     const expected = /*html*/`<tr>
-        <td>5c75a7b05ef279a15486ff42</td>
-        <td>Yes</td>
-        <td>2599.99</td>
+        <td>${singleContact.id}</td>
+        <td>${singleContact.isActive ? 'yes' : 'no'}</td>
+        <td>${singleContact.balance}</td>
         <td>
-            <img src="http://placehold.it/32x32">
+            <img src="${singleContact.picture}">
         </td>
-        <td>28 yo</td>
-        <td>Cantu</td>
-        <td>Sullivan</td>
-        <td>IDETICA</td>
-        <td>cantu.sullivan@idetica.com</td>
-        <td>banana</td>            
+        <td>${singleContact.age} yo</td>
+        <td>${singleContact.firstName}</td>
+        <td>${singleContact.lastName}</td>
+        <td>${singleContact.company}</td>
+        <td>${singleContact.email}</td>
+        <td>${singleContact.favoriteFruit}</td>            
         </tr>`;
 //assert
     assert.htmlEqual(result, expected);
