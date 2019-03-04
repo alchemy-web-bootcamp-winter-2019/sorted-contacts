1def43563f561f65366a1223c94115ae9893dd5e
Sean Nelson <smnelsonn@gmail.com>
2019-03-04 08:19:02 -0800

    convert functions to a single with sortChoice

--- test/sort-component.test.js
+++ test/sort-component.test.js
@@ -6,51 +6,72 @@ const contacts = [
     { firstName: 'Dave', age: 30 }
 ];
 
-function sortName(contacts) {
+// function sortName(contacts) {
+//     return contacts.sort((a, b) => {
+//         if(a.firstName === b.firstName) {
+//             return 0;
+//         }
+//         if(a.firstName > b.firstName) {
+//             return 1;
+//         }
+//         else {
+//             return -1;
+//         }
+//     });
+// }
+// function sortAge(contacts) {
+//     return contacts.sort((a, b) => {
+//         if(a.age === b.age) {
+//             return 0;
+//         }
+//         if(a.age > b.age) {
+//             return 1;
+//         }
+//         else {
+//             return -1;
+//         }
+//     });
+// }
+
+function sort(contacts, sortChoice) {
+    const property = sortChoice.property;
+
     return contacts.sort((a, b) => {
-        if(a.firstName === b.firstName) {
+        if(a[property] === b[property]) {
             return 0;
         }
-        if(a.firstName > b.firstName) {
+        if(a[property] > b[property]) {
             return 1;
         }
         else {
             return -1;
         }
     });
-}
-function sortAge(contacts) {
-    return contacts.sort((a, b) => {
-        if(a.age === b.age) {
-            return 0;
-        }
-        if(a.age > b.age) {
-            return 1;
-        }
-        else {
-            return -1;
-        }
-    });
-}
+}       
 
 test ('sort contacts by name', assert => {
+    const sortChoice = { property: 'firstName' };
+
     const expected = [ 
         { firstName: 'Cade', age: 40 },
         { firstName: 'Dave', age: 30 },
         { firstName: 'Ed', age: 20 }
     ];
-    const result = sortName(contacts);
+
+    const result = sort(contacts, sortChoice);
 
     assert.deepEqual(result, expected);
 });
 
 test ('sort contacts by age', assert => {
+    const sortChoice = { property: 'age' };
+
     const expected = [
         { firstName: 'Ed', age: 20 },
         { firstName: 'Dave', age: 30 },
         { firstName: 'Cade', age: 40 }
     ];
-    const result = sortAge(contacts);
+    const result = sort(contacts, sortChoice);
 
     assert.deepEqual(result, expected);
 });
